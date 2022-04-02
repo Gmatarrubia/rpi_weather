058b3107999eb49bfcce8c1937bbce576b2f1447
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2022-04-03 00:06:03 +0200

    install vscode and its extensions

--- installDevEnv.sh
+++ installDevEnv.sh
@@ -10,7 +10,22 @@ function check_ansible(){
     fi
 }
 
+function check_vscode(){
+    if [ -n "$(which code)" ]
+    then
+        echo "VS Code already installed"
+    else
+        echo "Installing ansible..."
+        wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+        sudo apt install -y code
+        code --install-extension Dart-Code.dart-code Dart-Code.flutter mhutchie.git-graph ms-vscode-remote.remote-containers ms-vscode-remote.remote-wsl
+        code --install-extension ms-vscode-remote.vscode-remote-extensionpack timonwong.shellcheck
+    fi
+}
+
 ## Script entry
+check_vscode
 check_ansible
 
 sudo ansible-playbook installDevEnv.yml
