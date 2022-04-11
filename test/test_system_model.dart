fc6afe3ded98d800b156a6b444fee879e1077213
Gonzalo Matarrubia <gonzalomatarrubia@hotmail.com>
2022-04-11 20:16:31 +0200

    implementation of system_model test

---
+++ test/test_system_model.dart
@@ -0,0 +1,37 @@
+import 'package:test/test.dart';
+import 'package:rpi_weather/models/system_model.dart';
+import 'package:intl/intl.dart' show toBeginningOfSentenceCase;
+
+void main() {
+  test('Set Location in model', () {
+    SystemModel model = SystemModel();
+    expect(model.getLocation(), "");
+
+    String city = "madrid";
+    model.setLocation(city);
+    expect(model.getLocation(), toBeginningOfSentenceCase(city));
+  });
+
+  test('Set WeatherData in model', () {
+    SystemModel model = SystemModel();
+    expect(model.temperature, 0.0);
+    expect(model.humidity, 0);
+    expect(model.weatherValue, 0);
+    expect(model.iconValue, "");
+    expect(model.weatherDescription, "");
+
+    double temperature = 20.0;
+    int humidity = 40;
+    dynamic weatherData;
+    weatherData = {
+      'main': {'temp': temperature, 'humidity': humidity},
+      'weather': [
+        {'id': 0, 'icon': "", 'description': ""}
+      ]
+    };
+
+    model.setWeatherData(weatherData);
+    expect(model.temperature, temperature);
+    expect(model.humidity, humidity);
+  });
+}
