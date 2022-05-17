5ad671acb3933579f30c806bce06318781e966f1
Gonzalo Matarrubia <gonzalomatarrubia@hotmail.com>
2022-05-17 15:23:58 +0200

    minor changes

--- installDevEnv.sh
+++ installDevEnv.sh
@@ -6,14 +6,6 @@ currentPath="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source ./checkFunctions.sh
 
 ## Script entry
-
-## Script entry
-if [ -z "${INSIDE_DOCKER}" ]
-then
-    check_vscode
-else
-    echo "Not needed vscode inside docker. Skiping!"
-fi
 check_ansible
 
 sudo ansible-playbook installDevEnv.yml
