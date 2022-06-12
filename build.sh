a3a630471ccd32097205b0cce9967ff122d06bb8
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2022-06-12 08:27:49 +0200

    build script runs test

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ repoPath="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 
 # Script arguments handle
 __verbose=
-__cmd=
+__test=
 
 while (( $# )); do
     case ${1,,} in
@@ -12,10 +12,10 @@ while (( $# )); do
             __verbose=1
             echo "Verbose output enabled"
             ;;
-        -cmd|--cmd)
+        -t|--test)
             shift
-            __cmd=("${@}")
-            echo Custom bitbake command: "${__cmd[@]}"
+            echo "Testing mode launched"
+            __test=1
             ;;
     esac
     shift
@@ -30,11 +30,11 @@ fi
 
 ###Enable option for only start
 
-if [ -z "${__cmd[*]}" ]
+if [ -z "$__test" ]
 then
     /opt/flutter/bin/flutter pub get
     time /opt/flutter/bin/flutter build linux
 else
-    echo "Executing command: ${__cmd[*]}"
-    time "${__cmd[@]}"
+    /opt/flutter/bin/flutter pub run build_runner build --delete-conflicting-outputs
+    time /opt/flutter/bin/flutter test
 fi
