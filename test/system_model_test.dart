ca29a1f4b847127b7a9c95a74bd7fbeffd2ed7a8
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2022-06-28 10:21:42 +0200

    fix tests

--- test/system_model_test.dart
+++ test/system_model_test.dart
@@ -19,15 +19,18 @@ void main() {
     expect(model.weatherValue, 0);
     expect(model.iconValue, "");
     expect(model.weatherDescription, "");
+    expect(model.timeZone, 0);
 
     double temperature = 20.0;
     int humidity = 40;
+    int timezone = 7200000;
     dynamic weatherData;
     weatherData = {
       'main': {'temp': temperature, 'humidity': humidity},
       'weather': [
         {'id': 0, 'icon': "", 'description': ""}
-      ]
+      ],
+      'timezone' : timezone,
     };
 
     model.setWeatherData(weatherData);
